dcba6d8411ba4f4480941792dca9eb7513b5e932
peruyeroj <peruyero@udel.edu>
2023-04-30 03:52:22 -0400

    third commit

--- quiz.js
+++ quiz.js
@@ -12,17 +12,25 @@ quizForm.addEventListener("submit", function(event) {
   ];
 
   let score = 0;
+  let feedback = "";
+  
   for (let i = 0; i < userAnswers.length; i++) {
     if (userAnswers[i] === correctAnswers[i]) {
       score++;
+    } else {
+      feedback += "Question " + (i+1) + ": ";
+      feedback += "Your answer was " + userAnswers[i] + ". ";
+      feedback += "<br>"; 
+      feedback += "The correct answer was " + correctAnswers[i] + ". ";
+      feedback += "Here's an explanation of why the correct answer is correct.";
+      feedback += "<br>";
     }
   }
 
-  let feedback = "";
   if (score === userAnswers.length) {
     feedback = "Congratulations! You got all the answers right.";
   } else {
-    feedback = "Sorry, you got " + score + " out of " + userAnswers.length + " answers correct. You need help.";
+    feedback = "Sorry, you got " + score + " out of " + userAnswers.length + " answers correct. Here's some feedback on your answers: <br>" + feedback;
   }
 
   feedbackDiv.innerHTML = feedback;
