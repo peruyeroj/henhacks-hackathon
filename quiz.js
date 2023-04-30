fac28e237032fb8530dd797f07cedb2b6c04802b
peruyeroj <peruyero@udel.edu>
2023-04-30 11:29:19 -0400

    fourth commit

--- quiz.js
+++ quiz.js
@@ -1,14 +1,15 @@
 const quizForm = document.getElementById("quiz-form");
 const feedbackDiv = document.getElementById("feedback");
 
-const correctAnswers = ["a", "b"];
+const correctAnswers = ["a", "b","c"];
 
 quizForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
   const userAnswers = [
     quizForm.q1.value,
-    quizForm.q2.value
+    quizForm.q2.value,
+    quizForm.q3.value
   ];
 
   let score = 0;
@@ -22,7 +23,7 @@ quizForm.addEventListener("submit", function(event) {
       feedback += "Your answer was " + userAnswers[i] + ". ";
       feedback += "<br>"; 
       feedback += "The correct answer was " + correctAnswers[i] + ". ";
-      feedback += "Here's an explanation of why the correct answer is correct.";
+      feedback += "Perhaps it's a good idea for you to refresh your knowledge of tree traversals by reading through the tree traversals section. ";
       feedback += "<br>";
     }
   }
